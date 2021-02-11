9bca8b06404892955098e300f97572012171de7a
1juann3 <1juannx3@gmail.com>
2021-02-11 18:37:30 -0500

    Changed if statements and fix timer not stopping

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
     
-    var counter = 30
+    var counter: Int = 0
 
-
-
+    var timer: Timer?
+    
     @objc func updateCounter() {
         //example functionality
         if counter > 0 {
@@ -24,32 +24,15 @@ class ViewController: UIViewController {
             counter -= 1
         }
     }
+    func stopTimerTest() {
+      timer?.invalidate()
+      timer = nil
+    }
 
     @IBAction func hardnessSelector(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        stopTimerTest()
+        counter = eggTimes[sender.currentTitle!]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         
-        if hardness == "Soft" {
-            counter = eggTimes["Soft"]!
-            print(eggTimes["Soft"]!)
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-            return
-        }
-        
-        if hardness == "Medium" {
-            counter = eggTimes["Medium"]!
-            print(eggTimes["Medium"]!)
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-            return
-        }
-        
-        else {
-            counter = eggTimes["Hard"]!
-            print(eggTimes["Hard"]!)
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        }
     }
-    
-    
-    
-    
 }
